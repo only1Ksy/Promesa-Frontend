863feeab2000d2ab9e43619b562997cad0636bf1
KWONDU <go203302@gmail.com>
2025-06-24 16:39:47 +0900

    refactor: open animation

--- src/components/features/artist/artist-question-section.tsx
+++ src/components/features/artist/artist-question-section.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import clsx from 'clsx';
 
 import { QA_LIST } from '@/lib/constants/qa-list';
 import DropUpIcon from '@/public/icons/artist/drop-up.svg';
@@ -15,13 +14,29 @@ interface ArtistQuestionSectionProps {
 }
 
 export default function ArtistQuestionSection({ artistId }: ArtistQuestionSectionProps) {
-  const [open, setOpen] = useState<boolean[]>(Array(QA_LIST.length).fill(false));
+  const [opens, setOpens] = useState<boolean[]>(Array(QA_LIST.length).fill(false));
+  const currentRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const { data: information, isLoading } = useQuery({
     queryKey: ['artistInformation', artistId],
     queryFn: () => fetchArtistInformation(artistId),
     select: (res) => res.data,
   });
+
+  useEffect(() => {
+    const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+    currentRefs.current.forEach((el, idx) => {
+      if (!el) return;
+
+      if (opens[idx]) {
+        const scrollHeight = el.scrollHeight;
+        el.style.maxHeight = scrollHeight / rootFontSize + 'rem';
+      } else {
+        el.style.maxHeight = 'calc(var(--spacing) * 6)';
+      }
+    });
+  }, [opens]);
 
   if (!information) return null;
 
@@ -30,33 +45,33 @@ export default function ArtistQuestionSection({ artistId }: ArtistQuestionSectio
   const { artistName } = information;
 
   const toggleOpen = (idx: number) => {
-    setOpen((prev) => prev.map((val, i) => (i === idx ? !val : val)));
+    setOpens((prev) => prev.map((val, i) => (i === idx ? !val : val)));
   };
 
   return (
     <div className="mx-5 flex flex-col">
       <div className="text-subhead font-medium text-black">{`${artistName} 작가에게 묻습니다`}</div>
-      <div className="mt-6 mb-4 flex flex-col gap-3">
+      <div className="mx-1 mt-6 mb-4 flex flex-col gap-3">
         {QA_LIST.map(({ question, answer }, idx) => (
-          <React.Fragment key={idx}>
-            <div className="flex items-start justify-between">
-              <span
-                className={clsx(
-                  'text-body-02 text-grey-7 custom-break-words max-w-65 font-medium',
-                  open[idx] ? '' : 'line-clamp-1 overflow-hidden text-ellipsis',
-                )}
-              >
-                {question}
-              </span>
-              <button onClick={() => toggleOpen(idx)} className="cursor-pointer">
-                {open[idx] ? <DropUpIcon className="text-grey-5" /> : <PlusIcon className="text-grey-5" />}
-              </button>
+          <div key={idx} className="flex flex-col gap-3">
+            <div
+              ref={(el) => {
+                currentRefs.current[idx] = el;
+              }}
+              className="flex flex-col gap-3 overflow-hidden transition-all ease-in-out"
+            >
+              <div className="flex items-start justify-between">
+                <span className="text-body-02 text-grey-7 custom-break-words max-w-65 font-medium text-ellipsis">
+                  {question}
+                </span>
+                <button onClick={() => toggleOpen(idx)} className="cursor-pointer">
+                  {opens[idx] ? <DropUpIcon className="text-grey-5" /> : <PlusIcon className="text-grey-5" />}
+                </button>
+              </div>
+              <span className="text-caption-01 text-grey-7 custom-break-words mr-4.5 mb-3 font-medium">{answer}</span>
             </div>
-            {open[idx] && (
-              <span className="text-caption-01 text-grey-7 custom-break-words max-w-84 font-medium">{answer}</span>
-            )}
             <hr className="border-grey-4 w-full" />
-          </React.Fragment>
+          </div>
         ))}
       </div>
 
