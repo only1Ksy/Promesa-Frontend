e44998509cc8b52ef2ab05a80d63972cf76c58c4
only1Ksy <winterksy@gmail.com>
2025-06-28 12:27:32 +0900

    refactor: separate detail/review, review page

---
+++ src/components/features/detail/review-preview.tsx
@@ -0,0 +1,57 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+
+import ReviewCard from '@/components/common/review/review-card';
+import { fetchItemDetail } from '@/services/api/item';
+import { Review } from '@/types/review.dto';
+
+interface ReviewListProps {
+  reviews: Review[];
+  itemId: number;
+}
+
+export default function ReviewList({ reviews, itemId }: ReviewListProps) {
+  const router = useRouter();
+
+  const { data: item } = useQuery({
+    queryKey: ['itemDetail', itemId],
+    queryFn: () => fetchItemDetail(itemId),
+    select: (res) => res.data,
+  });
+
+  if (!item) return null;
+
+  const visibleReviews = reviews.slice(0, 2);
+
+  const showAll = () => {
+    router.push(`/review/${itemId}`);
+  };
+
+  return (
+    <div className="relative flex flex-col items-center">
+      <div className="mb-10 flex w-full flex-col gap-5">
+        {visibleReviews.map((review, i) => (
+          <div key={i} className="flex w-full flex-col items-center gap-5">
+            <ReviewCard {...review} />
+            {i !== visibleReviews.length - 1 && <div className="border-deep-green h-0 w-90.5 border-b" />}
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={showAll}
+        className="border-grey-9 text-body-02 text-grey-9 flex h-11 w-80.5 cursor-pointer items-center justify-center border font-bold"
+      >
+        전체 리뷰 보기
+      </button>
+
+      {/* 그라데이션 덮개 */}
+      <div
+        className="pointer-events-none absolute bottom-15 left-0 z-10 h-13.5 w-full"
+        style={{
+          background: 'linear-gradient(180deg, rgba(238, 239, 233, 0.00) -50%, #EEEFE9 100%)',
+        }}
+      />
+    </div>
+  );
+}
---
+++ src/components/features/review/review-list.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import { Review } from '@/types/review.dto';
+
+import ReviewCard from '../../common/review/review-card';
+import ReviewPagination from './review-pagination';
+
+interface ReviewListProps {
+  reviews: Review[];
+}
+
+export default function ReviewList({ reviews }: ReviewListProps) {
+  const REVIEWS_PER_PAGE = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPage = Math.ceil(reviews.length / REVIEWS_PER_PAGE);
+  const visibleReviews = reviews.slice((currentPage - 1) * REVIEWS_PER_PAGE, currentPage * REVIEWS_PER_PAGE);
+
+  return (
+    <div className="relative flex flex-col items-center">
+      <div className="mb-10 flex w-full flex-col gap-5">
+        {visibleReviews.map((review, i) => (
+          <div key={i} className="flex w-full flex-col items-center gap-5">
+            <ReviewCard {...review} />
+            {i !== visibleReviews.length - 1 && <div className="border-deep-green h-0 w-90.5 border-b" />}
+          </div>
+        ))}
+      </div>
+
+      {totalPage > 1 && (
+        <ReviewPagination currentPage={currentPage} totalPage={totalPage} onPageChange={setCurrentPage} />
+      )}
+    </div>
+  );
+}
