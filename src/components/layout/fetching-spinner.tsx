42f8e6971956e1a83600dbd736a545bb418a77b5
KWONDU <go203302@gmail.com>
2025-07-02 00:10:23 +0900

    feat: new loading effect

--- src/components/layout/fetching-spinner.tsx
+++ src/components/layout/fetching-spinner.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from 'react';
 
+import VideoLoader from '@/components/layout/video-loader';
+
 export default function FetchingSpinner() {
   // block scroll
   useEffect(() => {
@@ -15,7 +17,7 @@ export default function FetchingSpinner() {
 
   return (
     <div className="fixed-component max-z-index top-0 flex min-h-screen items-center justify-center bg-black/20">
-      <div id="loader" />
+      <VideoLoader />
     </div>
   );
 }
