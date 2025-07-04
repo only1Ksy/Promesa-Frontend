a8600314854b2218995d85084c34932e3680b373
KWONDU <go203302@gmail.com>
2025-07-05 01:13:18 +0900

    fix: solve hydration error

--- src/components/common/utilities/image-with-loading.tsx
+++ src/components/common/utilities/image-with-loading.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Image, { ImageProps } from 'next/image';
 
 import { useImageLoadingStore } from '@/lib/store/use-image-loading-store';
@@ -8,11 +8,17 @@ import { useImageLoadingStore } from '@/lib/store/use-image-loading-store';
 export default function ImageWithLoading({ src, alt, onLoad, ...rest }: ImageProps) {
   const startLoading = useImageLoadingStore((s) => s.startLoading);
   const endLoading = useImageLoadingStore((s) => s.endLoading);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     startLoading();
     return endLoading;
   }, [src, startLoading, endLoading]);
+
+  if (!isClient) {
+    return <div className="absolute inset-0 h-full w-full" />;
+  }
 
   return (
     <Image
