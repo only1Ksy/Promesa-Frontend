40826186e1be66fd59e86273e37613c41afa0200
KWONDU <go203302@gmail.com>
2025-07-13 14:32:57 +0900

    feat: add status to error page

--- src/types/axios.dto.ts
+++ src/types/axios.dto.ts
@@ -2,3 +2,13 @@ export interface APIErrorResponse {
   code: string;
   reason: string;
 }
+
+export class HttpError extends Error {
+  status?: number;
+
+  constructor(message: string, status?: number) {
+    super(message);
+    this.name = 'HttpError';
+    this.status = status;
+  }
+}
