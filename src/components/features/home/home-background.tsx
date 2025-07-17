2fefea21068b847a93e34b11308e1697218865b4
Donguk Kwon <70671704+KWONDU@users.noreply.github.com>
2025-07-18 05:41:56 +0900

    fix: add will-change

--- src/components/features/home/home-background.tsx
+++ src/components/features/home/home-background.tsx
@@ -18,7 +18,7 @@ export default function HomeBackground() {
       <Link href="/shop">
         <div
           className={clsx(
-            'text-body-02 font-regular text-grey-1 border-grey-1 z-5 bg-transparent transition-colors duration-200',
+            'text-body-02 font-regular text-grey-1 border-grey-1 z-5 bg-transparent transition-colors duration-200 will-change-transform',
             'only-hover:bg-grey-1 only-hover:text-grey-6 active:bg-grey-1 active:text-grey-6',
             'flex h-8 w-35 items-center justify-center rounded-[40px] border-[1.2px] backdrop-blur-[2.35px]',
           )}
