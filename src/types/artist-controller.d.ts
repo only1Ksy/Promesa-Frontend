78f93b6a1a5948d623c1b68ccbefea6db909b418
KWONDU <go203302@gmail.com>
2025-07-18 00:54:16 +0900

    feat: artist list, artist name list api

--- src/types/artist-controller.d.ts
+++ src/types/artist-controller.d.ts
@@ -4,3 +4,10 @@ export interface ArtistResponseSchema {
   profile: ArtistProfileSchema;
   wish: ArtistWishSchema;
 }
+
+export interface ArtistNameResponseSchema {
+  id: number;
+  name: string;
+  englishName: string;
+  artistPageUrl: string;
+}
