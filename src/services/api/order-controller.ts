8f22d76691fdbc231b05bc4e03d7cd138e992c2d
only1Ksy <winterksy@gmail.com>
2025-07-24 00:05:29 +0900

    feat: set default address at order page function

---
+++ src/services/api/order-controller.ts
@@ -0,0 +1,31 @@
+import { AddressSchema } from '@/types/address-controller';
+
+import { withErrorBoundary } from './axios/instance';
+import { axiosInstance } from './axios/instance';
+
+/** 이름, 우편번호, 메인주소, 서브주소, 핸드폰번호를 전달하면 기본 배송지로 설정하는 함수 */
+export const PostReviewImages = (
+  recipientName: string,
+  zipCode: string,
+  addressMain: string,
+  addressDetails: string,
+  recipientPhone: string,
+) =>
+  withErrorBoundary<[string, string, string, string, string], AddressSchema>(
+    async (recipientName, zipCode, addressMain, addressDetails, recipientPhone) => {
+      const res = await axiosInstance.post('/orders/address', {
+        recipientName,
+        zipCode,
+        addressMain,
+        addressDetails,
+        recipientPhone,
+      });
+
+      return res.data.data;
+    },
+    recipientName,
+    zipCode,
+    addressMain,
+    addressDetails,
+    recipientPhone,
+  );
