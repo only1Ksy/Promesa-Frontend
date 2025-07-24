f239fcb96c971472b4780e49fcc6cee430e5b62a
only1Ksy <winterksy@gmail.com>
2025-07-24 15:00:51 +0900

    feat: add edit, delete btn to my review written component

--- src/components/features/my/review/my-review-written.tsx
+++ src/components/features/my/review/my-review-written.tsx
@@ -27,20 +27,40 @@ export default function MyReviewWritten() {
     },
   ];
 
+  const editReview = () => {};
+
+  const deleteReview = () => {};
+
   return (
     <>
       <div className="flex w-full flex-col gap-8.75">
         {REVIEW_TEMP.map((review, index) => (
           <>
-            <div key={index} className="flex flex-col items-center justify-center gap-6.5 px-5">
-              <MyReviewProductCard
-                url={review.url}
-                artistName={review.artistName}
-                title={review.title}
-                itemCount={review.itemCount}
-                date={review.date}
-              />
-              <MyReviewCard rating={review.rate} content={review.reviewText} reviewImages={review.reviewImages} />
+            <div className="flex flex-col gap-5 px-5">
+              <div key={index} className="flex flex-col items-center justify-center gap-6.5">
+                <MyReviewProductCard
+                  url={review.url}
+                  artistName={review.artistName}
+                  title={review.title}
+                  itemCount={review.itemCount}
+                  date={review.date}
+                />
+                <MyReviewCard rating={review.rate} content={review.reviewText} reviewImages={review.reviewImages} />
+              </div>
+              <div className="text-grey-9 text-body-02 flex items-center gap-1.5 font-medium">
+                <button
+                  onClick={editReview}
+                  className="flex h-8.75 w-24.25 cursor-pointer items-center justify-center rounded-xs border"
+                >
+                  수정
+                </button>
+                <button
+                  onClick={deleteReview}
+                  className="flex h-8.75 w-24.25 cursor-pointer items-center justify-center rounded-xs border"
+                >
+                  삭제
+                </button>
+              </div>
             </div>
             {index !== REVIEW_TEMP.length - 1 && <div className="bg-green h-[1px] w-full" />}
           </>
