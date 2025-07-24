5b2c24e4eba8abb6ffcb4cf838bdb49bfc200856
only1Ksy <winterksy@gmail.com>
2025-07-24 10:11:07 +0900

    feat: my review toggle component

--- src/components/client/my/review/page.tsx
+++ src/components/client/my/review/page.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { useState } from 'react';
+
+import MyReviewAvailable from '@/components/features/my/review/my-review-available';
+import MyReviewToggle from '@/components/features/my/review/my-review-toggle';
+import MyReviewWritten from '@/components/features/my/review/my-review-written';
+
 export default function ClientMyReviewPage() {
-  return <></>;
+  const [activeTab, setActiveTab] = useState<'available' | 'written'>('available');
+
+  return (
+    <>
+      <div className="sticky top-11.5 z-40">
+        <MyReviewToggle onSelect={setActiveTab} active={activeTab} />
+      </div>
+      <div>{activeTab === 'available' ? <MyReviewAvailable /> : <MyReviewWritten />}</div>
+    </>
+  );
 }
