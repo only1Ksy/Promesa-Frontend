ce5044d458b02b2a30daf094442ff38b5b56009e
only1Ksy <winterksy@gmail.com>
2025-07-24 11:55:00 +0900

    feat: my review written component

--- src/components/features/my/review/my-review-written.tsx
+++ src/components/features/my/review/my-review-written.tsx
@@ -1,3 +1,51 @@
+import MyReviewCard from './my-review-card';
+import MyReviewProductCard from './my-review-product-card';
+
 export default function MyReviewWritten() {
-  return <></>;
+  const REVIEW_TEMP = [
+    {
+      url: '/src/review',
+      artistName: '김영은',
+      title: '빈티지 블랙 높은 잔 세트',
+      itemCount: 5,
+      date: '2025.05.25',
+      rate: 4,
+      reviewText:
+        '디자인도 흔하지 않고, 너무 귀여워요. 근데 사이즈가 너무 커서 핸드폰에 달면 좀 불편하긴 합니다. 그래도 예뻐서 만족해요. 굿굿!! 디자인도 흔하지 않고, 너무 귀여워요. 근데 사이즈가 너무 커서 핸드폰에 달면 좀 불편하긴 합니다. 그래도 예뻐서 만족해요 ',
+      reviewImages: ['/src/review1', '/src/review2', 'src/review3'],
+    },
+    {
+      url: '/src/review',
+      artistName: '김영은',
+      title: '빈티지 블랙 높은 잔 세트',
+      itemCount: 5,
+      date: '2025.05.25',
+      rate: 4,
+      reviewText:
+        '디자인도 흔하지 않고, 너무 귀여워요. 근데 사이즈가 너무 커서 핸드폰에 달면 좀 불편하긴 합니다. 그래도 예뻐서 만족해요. 굿굿!! 디자인도 흔하지 않고, 너무 귀여워요. 근데 사이즈가 너무 커서 핸드폰에 달면 좀 불편하긴 합니다. 그래도 예뻐서 만족해요 ',
+      reviewImages: [],
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex w-full flex-col gap-8.75">
+        {REVIEW_TEMP.map((review, index) => (
+          <>
+            <div key={index} className="flex flex-col items-center justify-center gap-6.5 px-5">
+              <MyReviewProductCard
+                url={review.url}
+                artistName={review.artistName}
+                title={review.title}
+                itemCount={review.itemCount}
+                date={review.date}
+              />
+              <MyReviewCard rating={review.rate} content={review.reviewText} reviewImages={review.reviewImages} />
+            </div>
+            {index !== REVIEW_TEMP.length - 1 && <div className="bg-green h-[1px] w-full" />}
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
