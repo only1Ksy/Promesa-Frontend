0c0c8b3d340c6ca884930f30aa9a2203db9bff4b
only1Ksy <winterksy@gmail.com>
2025-07-26 20:53:17 +0900

    feat: post, patch, delete carts function

--- src/services/api/cart-controller.ts
+++ src/services/api/cart-controller.ts
@@ -10,8 +10,23 @@ export const fetchCarts = () =>
     return res.data.data;
   });
 
-/** itemId, quantity를 전달하면 아이템을 장바구니에 추가하는 함수 */
-export const postCarts = async (cartData: CartRequest): Promise<CartResponse> => {
-  const res = await axiosInstance.post('/carts', cartData);
-  return res.data.data;
-};
+/** itemId, quantity를 전달하면 아이템을 장바구니에 추가, 수량을 증가시키는 함수 */
+export const postCarts = (cartsData: CartRequest) =>
+  withErrorBoundary<[CartRequest], CartSchema>(async (cartsData) => {
+    const res = await axiosInstance.post('/carts', cartsData);
+    return res.data.data;
+  }, cartsData);
+
+/** itemId, quantity를 전달하면 아이템 수량을 전달한 quantity로 수정하는 함수 */
+export const patchCarts = (cartsData: CartRequest) =>
+  withErrorBoundary<[CartRequest], CartSchema>(async (cartsData) => {
+    const res = await axiosInstance.patch('/carts', cartsData);
+    return res.data.data;
+  }, cartsData);
+
+/** itemId를 전달하면 아이템을 장바구니에서 삭제하는 함수 */
+export const deleteCarts = (itemId: number) =>
+  withErrorBoundary<[number], CartSchema>(async (itemId) => {
+    const res = await axiosInstance.delete(`/carts/${itemId}`);
+    return res.data.data;
+  }, itemId);
