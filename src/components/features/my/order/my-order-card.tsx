29ecd6e59b5027eacfb635bbcdccb24b5767806f
only1Ksy <winterksy@gmail.com>
2025-07-27 00:48:35 +0900

    design: my order card

--- src/components/features/my/order/my-order-card.tsx
+++ src/components/features/my/order/my-order-card.tsx
@@ -17,15 +17,15 @@ export default function MyOrderCard({ status, shipComment, url, title, price, it
   return (
     <div className="flex flex-col gap-4.5">
       <div className="flex flex-col gap-3.5">
-        <div className="flex items-center gap-3 px-5">
-          <span className="text-body-01 text-orange font-bold overflow-ellipsis">{status}</span>
-          <span className="text-grey-6 text-body-01 font-medium overflow-ellipsis">{shipComment}</span>
-        </div>
-        <div className="flex items-center gap-3.5 px-5">
-          <div className="h-23 w-23">
+        <div className="flex gap-3.5 px-5">
+          <div className="h-28.75 w-23">
             <ImageWithEffect fill src={url} alt={'상품 미리보기 이미지'} className="object-cover" />
           </div>
           <div className="flex flex-col gap-1.5">
+            <div className="flex items-center gap-3">
+              <span className="text-body-01 text-orange font-bold overflow-ellipsis">{status}</span>
+              <span className="text-grey-6 text-body-01 font-medium overflow-ellipsis">{shipComment}</span>
+            </div>
             <span className="text-body-01 text-grey-9 font-bold overflow-ellipsis">{title}</span>
             <span className="text-body-02 text-grey-6 font-medium">{`${price.toLocaleString()}원 / 수량: ${itemCount}개`}</span>
           </div>
