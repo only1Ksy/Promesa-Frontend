7ec36c05327d10e932c75a314acfe351c7e6d603
KWONDU <go203302@gmail.com>
2025-07-27 18:01:15 +0900

    feat: search API

--- src/services/api/home-controller.ts
+++ src/services/api/home-controller.ts
@@ -1,6 +1,12 @@
-import type { BrandInfoResponseSchema } from '@/types/home-controller';
+import type { BrandInfoResponseSchema, SearchResponseSchema } from '@/types/home-controller';
 
 import { axiosInstance, withErrorBoundary } from './axios/instance';
+
+export const fetchSearch = (keyword: string) =>
+  withErrorBoundary<[string], SearchResponseSchema>(async (keyword) => {
+    const res = await axiosInstance.get(`/search?keyword=${keyword}`);
+    return res.data.data;
+  }, keyword);
 
 export const fetchBrandInfo = () =>
   withErrorBoundary<[], BrandInfoResponseSchema>(async () => {
--- src/types/home-controller.d.ts
+++ src/types/home-controller.d.ts
@@ -1,6 +1,14 @@
+import { ArtistResponseSchema } from './artist-controller';
+import { ItemPreviewResponseSchema } from './item-controller';
+
 export interface BrandInfoResponseSchema {
   mainImageUrl: string;
   leftImageUrl: string;
   rightImageUrl: string;
   brandStory: string;
+}
+
+export interface SearchResponseSchema {
+  artists: ArtistResponseSchema[];
+  itemPreviews: ItemPreviewResponseSchema[];
 }
