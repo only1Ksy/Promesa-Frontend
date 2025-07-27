d39e1ad747cd031ff4911240c8c8fd4e94fbac55
only1Ksy <winterksy@gmail.com>
2025-07-27 21:54:37 +0900

    fix: order product card image size

--- src/components/features/order/product-card.tsx
+++ src/components/features/order/product-card.tsx
@@ -14,14 +14,16 @@ interface ItemProps {
 
 export default function ProductCard({ item }: ItemProps) {
   return (
-    <div className="flex items-center gap-4.5">
-      <div className="h-23 w-23">
+    <div className="flex gap-4.5">
+      <div className="h-28.75 w-23">
         <ImageWithEffect src={item.imageSrc} alt={'ProductPreviewImage'} fill className="object-cover" />
       </div>
-      <div className="flex flex-col">
-        <div className="text-body-02 font-bold">{item.artistName}</div>
-        <div className="text-body-01 font-medium">{item.itemName}</div>
-        <div className="text-body-02 font-medium">{item.itemNumber}개</div>
+      <div className="flex flex-col gap-1.5">
+        <div className="flex flex-col">
+          <div className="text-body-02 font-bold">{item.artistName}</div>
+          <div className="text-body-01 font-medium">{item.itemName}</div>
+          <div className="text-body-02 font-medium">{item.itemNumber}개</div>
+        </div>
         <div className="text-body-01 font-bold">{(item.price * item.itemNumber).toLocaleString()}원</div>
       </div>
     </div>
