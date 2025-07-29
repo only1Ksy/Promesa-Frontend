4536f3d60cd1a840b1494cbc78316f7cba08c13d
only1Ksy <winterksy@gmail.com>
2025-07-29 11:46:00 +0900

    design: minor  change

--- src/components/common/alert/toast-provider.tsx
+++ src/components/common/alert/toast-provider.tsx
@@ -17,18 +17,31 @@ export const useToast = () => {
 export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
   const [message, setMessage] = useState('');
   const [visible, setVisible] = useState(false);
+  const [isHiding, setIsHiding] = useState(false);
 
   const showToast = (msg: string) => {
     setMessage(msg);
     setVisible(true);
-    setTimeout(() => setVisible(false), 2000);
+    setIsHiding(false);
+
+    setTimeout(() => {
+      setIsHiding(true);
+      setTimeout(() => {
+        setVisible(false);
+        setMessage('');
+      }, 500);
+    }, 1800);
   };
 
   return (
     <ToastContext.Provider value={{ showToast }}>
       {children}
       {visible && (
-        <div className="text-body-02 fixed bottom-40 left-1/2 z-9999 -translate-x-1/2 rounded-sm bg-black/70 px-4 py-2 text-white transition-opacity duration-500">
+        <div
+          className={`text-body-02 fixed bottom-40 left-1/2 z-50 -translate-x-1/2 rounded-sm bg-black/70 px-4 py-2 text-white transition-opacity duration-500 ${
+            isHiding ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
           {message}
         </div>
       )}
