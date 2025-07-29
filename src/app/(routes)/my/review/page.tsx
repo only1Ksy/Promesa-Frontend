1dd15f70b0a587bf3cdf7d61bda641b4c9adf38b
only1Ksy <winterksy@gmail.com>
2025-07-29 12:03:47 +0900

    feat: apply apis to my review page

--- src/app/(routes)/my/review/page.tsx
+++ src/app/(routes)/my/review/page.tsx
@@ -1,12 +1,16 @@
-// import { dehydrate } from '@tanstack/react-query';
+import { dehydrate } from '@tanstack/react-query';
+
 import ClientMyReviewPage from '@/components/client/my/review/page';
+import { fetchMyEligibleReviews, fetchMyWrittenReviews } from '@/services/api/review-controller';
+import { createQueryClient } from '@/services/query/server';
 
 export default async function MyPage() {
-  /* const queryClient = createQueryClient();
+  const queryClient = createQueryClient();
 
-  await Promise.all([queryClient.prefetchQuery({ queryKey: ['me'], queryFn: fetchMe })]);
+  await Promise.all([queryClient.prefetchQuery({ queryKey: ['writtenReviews'], queryFn: fetchMyWrittenReviews })]);
+  await Promise.all([queryClient.prefetchQuery({ queryKey: ['eligibleReviews'], queryFn: fetchMyEligibleReviews })]);
 
-  const dehydratedState = dehydrate(queryClient); */
+  const dehydratedState = dehydrate(queryClient);
 
-  return <ClientMyReviewPage />;
+  return <ClientMyReviewPage myReviewsState={dehydratedState} />;
 }
