d5c4cf3930bc3dce5494035b846159ce6629ad82
KWONDU <go203302@gmail.com>
2025-07-31 04:17:22 +0900

    feat: member-controller API

--- src/services/api/member-controller.ts
+++ src/services/api/member-controller.ts
@@ -1,4 +1,4 @@
-import type { MemberResponseSchema } from '@/types/member-controller';
+import type { MemberResponseSchema, MemberUpdateRequestSchema } from '@/types/member-controller';
 
 import { axiosInstance, withErrorBoundary } from './axios/instance';
 
@@ -6,4 +6,16 @@ export const fetchMe = () =>
   withErrorBoundary<[], MemberResponseSchema>(async () => {
     const res = await axiosInstance.get('/auth/me');
     return res.data.data;
+  });
+
+export const patchMe = (payload: MemberUpdateRequestSchema) =>
+  withErrorBoundary<[MemberUpdateRequestSchema], MemberResponseSchema>(async () => {
+    const res = await axiosInstance.patch('/auth/me', payload);
+    return res.data.data;
+  }, payload);
+
+export const patchMeWithdraw = () =>
+  withErrorBoundary<[], boolean>(async () => {
+    const res = await axiosInstance.patch('/auth/me/withdraw');
+    return res.data.success;
   });
