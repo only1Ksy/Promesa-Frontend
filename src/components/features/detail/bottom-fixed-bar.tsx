7fdb8eda5d2afe264d56eba349f4662b4fcc7849
only1Ksy <winterksy@gmail.com>
2025-08-01 16:17:11 +0900

    feat: add show toast to bottom bar wish btn

--- src/components/features/detail/bottom-fixed-bar.tsx
+++ src/components/features/detail/bottom-fixed-bar.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { useToast } from '@/components/common/alert/toast-provider';
 import { useToggleWish } from '@/hooks/use-toggle-wish';
 import HeartEmptyIcon from '@/public/icons/item/heart-empty.svg';
 import HeartFilledIcon from '@/public/icons/item/heart-filled.svg';
@@ -17,6 +18,7 @@ interface BottomFixedBarProps {
 
 export default function BottomFixedBar({ item, wished, wishCount }: BottomFixedBarProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { showToast } = useToast();
 
   const onClicked = () => {
     setIsModalOpen(true);
@@ -24,15 +26,21 @@ export default function BottomFixedBar({ item, wished, wishCount }: BottomFixedB
 
   const { mutate: toggleWish } = useToggleWish();
 
+  const handleWish = () => {
+    if (wished) {
+      showToast('위시리스트에서 제거되었습니다.');
+    } else {
+      showToast('위시리스트에 추가했습니다.');
+    }
+    toggleWish({ targetType: 'ITEM', targetId: item.itemId, currentWished: wished });
+  };
+
   return (
     <>
       <div className="bg-pale-green border-green flex w-full flex-col gap-2 border px-5 pb-1 shadow-[0_0_60px_0_rgba(0,0,0,0.10)]">
         <div className="my-3 flex gap-3">
           <div className="flex h-12 w-12 flex-col items-center">
-            <button
-              onClick={() => toggleWish({ targetType: 'ITEM', targetId: item.itemId, currentWished: wished })}
-              className="cursor-pointer"
-            >
+            <button onClick={handleWish} className="cursor-pointer">
               {wished ? (
                 <>
                   <HeartFilledIcon width="32" height="32" className="text-orange" />
