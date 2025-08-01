f3f865687efc787109b1998748d0064e39bd96aa
only1Ksy <winterksy@gmail.com>
2025-08-01 16:29:25 +0900

    feat: add empty components to my order, my review page

--- src/components/features/my/order/my-order-list.tsx
+++ src/components/features/my/order/my-order-list.tsx
@@ -18,48 +18,58 @@ export default function MyOrderList({ orders }: MyOrderListProps) {
 
   const isButton = false;
 
-  return (
-    <>
-      {orders.map((order, index) => {
-        // 상태 텍스트 계산
-        const statusText = getOrderStatusText(order.orderStatus, order.deliveryStatus);
-        const shipComment = getShipComment(
-          order.deliveryStatus,
-          order.deliveryExpectedDate,
-          order.deliveryCompletedDate,
-        );
-        // 날짜 계산
-        const { year, month, day } = formatKoreanDateTime(order.orderDate);
+  const isMyOrdersEmpty = orders.length === 0;
 
-        return (
-          <>
-            <div key={index} className="pt-5 pb-6">
-              <div className="text-grey-9 text-body-02 flex items-center justify-between px-5 py-2 font-medium">
-                <span>
-                  {String(year)}.{String(month).padStart(2, '0')}.{String(day).padStart(2, '0')}
-                </span>
-                <button
-                  onClick={() => router.push(`/my/order?id=${order.orderId}`)}
-                  className="flex cursor-pointer items-center gap-1"
-                >
-                  <span>주문 상세</span>
-                  <GoIcon className="text-grey-5 rotate-90" />
-                </button>
+  if (isMyOrdersEmpty) {
+    return (
+      <p className="text-grey-6 text-body-01 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        작성 가능한 리뷰가 없어요
+      </p>
+    );
+  } else {
+    return (
+      <>
+        {orders.map((order, index) => {
+          // 상태 텍스트 계산
+          const statusText = getOrderStatusText(order.orderStatus, order.deliveryStatus);
+          const shipComment = getShipComment(
+            order.deliveryStatus,
+            order.deliveryExpectedDate,
+            order.deliveryCompletedDate,
+          );
+          // 날짜 계산
+          const { year, month, day } = formatKoreanDateTime(order.orderDate);
+
+          return (
+            <>
+              <div key={index} className="pt-5 pb-6">
+                <div className="text-grey-9 text-body-02 flex items-center justify-between px-5 py-2 font-medium">
+                  <span>
+                    {String(year)}.{String(month).padStart(2, '0')}.{String(day).padStart(2, '0')}
+                  </span>
+                  <button
+                    onClick={() => router.push(`/my/order?id=${order.orderId}`)}
+                    className="flex cursor-pointer items-center gap-1"
+                  >
+                    <span>주문 상세</span>
+                    <GoIcon className="text-grey-5 rotate-90" />
+                  </button>
+                </div>
+                <MyOrderCard
+                  status={statusText}
+                  shipComment={shipComment}
+                  url={order.itemThumbnail}
+                  title={order.totalQuantity > 1 ? `${order.itemName} 외 ${order.totalQuantity - 1}건` : order.itemName}
+                  price={order.totalAmount}
+                  itemCount={order.totalQuantity}
+                  isButton={isButton}
+                />
               </div>
-              <MyOrderCard
-                status={statusText}
-                shipComment={shipComment}
-                url={order.itemThumbnail}
-                title={order.totalQuantity > 1 ? `${order.itemName} 외 ${order.totalQuantity - 1}건` : order.itemName}
-                price={order.totalAmount}
-                itemCount={order.totalQuantity}
-                isButton={isButton}
-              />
-            </div>
-            {index !== orders.length - 1 && <div className="bg-green mx-5 h-[1px]" />}
-          </>
-        );
-      })}
-    </>
-  );
+              {index !== orders.length - 1 && <div className="bg-green mx-5 h-[1px]" />}
+            </>
+          );
+        })}
+      </>
+    );
+  }
 }
