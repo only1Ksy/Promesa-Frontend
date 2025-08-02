667b8458f2d99eb2db6ecd49255cabda4b3375c5
only1Ksy <winterksy@gmail.com>
2025-08-02 11:19:50 +0900

    feat: connect cart number to header

--- src/components/layout/header/cart-button.tsx
+++ src/components/layout/header/cart-button.tsx
@@ -1,25 +1,37 @@
 'use client';
 
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 
 import { useAuthStore } from '@/lib/store/use-auth-store';
 import CartIcon from '@/public/icons/layout/cart.svg';
+import { fetchCarts } from '@/services/api/cart-controller';
 
 export default function CartButton() {
   const { isLoggedIn, hasChecked, checkLogin } = useAuthStore();
+
+  const { data: carts, isLoading } = useQuery({
+    queryKey: ['carts'],
+    queryFn: () => fetchCarts(),
+    select: (res) => res,
+  });
 
   useEffect(() => {
     if (!hasChecked) checkLogin();
   }, [hasChecked, checkLogin]);
 
+  if (!carts || isLoading) return null;
+
+  const isNumber = isLoggedIn && carts.length !== 0;
+
   return (
     <div className="relative">
       <Link href="/cart">
         <CartIcon className="text-grey-9" />
-        {isLoggedIn && (
+        {isNumber && (
           <div className="bg-orange absolute right-0 bottom-0 flex h-4 w-4 items-center justify-center rounded-full">
-            <p className="text-caption-02 text-center font-medium text-white">3</p>
+            <p className="text-caption-02 text-center font-medium text-white">{carts.length}</p>
           </div>
         )}
       </Link>
