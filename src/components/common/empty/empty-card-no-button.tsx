7db3829576658a776dab02b1e8aba8b9cf6bbaf9
only1Ksy <winterksy@gmail.com>
2025-08-02 11:53:06 +0900

    feat: apply empty card

--- src/components/common/empty/empty-card-no-button.tsx
+++ src/components/common/empty/empty-card-no-button.tsx
@@ -4,12 +4,12 @@ interface EmptyCardProps {
   text: string;
 }
 
-export default function EmptyCard({ text }: EmptyCardProps) {
+export default function EmptyCardNoButton({ text }: EmptyCardProps) {
   return (
     <div className="absolute inset-0 flex h-full items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-5">
         <EmptyLogoIcon className="text-grey-5" />
-        <p className="text-subhead text-grey-9 font-bold">{text}</p>
+        <p className="text-subhead text-grey-5 font-bold">{text}</p>
       </div>
     </div>
   );
