8bff8951b4b85693f7cb2569b17dc18836de7f47
KWONDU <go203302@gmail.com>
2025-08-04 22:25:50 +0900

    feat: add detail page link

--- src/components/features/my/wish-list/wish-list-item.tsx
+++ src/components/features/my/wish-list/wish-list-item.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import ImageWithEffect from '@/components/common/utilities/image-with-effect';
 import type { WishResponseSchema } from '@/types/wish-controller';
 
@@ -19,18 +21,20 @@ export default function WishListItem({ item, isSelected, select }: WishListItemP
           )}
         </button>
       </div>
-      <div className="flex gap-3.5">
-        <div className="bg-green aspect-[4/5]">
-          <ImageWithEffect src={item.thumbnailUrl} alt={`아이템 ${item.targetId} 프리뷰 이미지.`} fill />
-        </div>
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col">
-            <p className="text-body-02 text-grey-9 custom-break-words font-medium">{item.title}</p>
-            <p className="text-caption-01 text-grey-6 font-medium">{item.artistName}</p>
+      <Link href={`/detail/${item.targetId}`}>
+        <div className="flex h-full gap-3.5">
+          <div className="bg-green relative aspect-[4/5]">
+            <ImageWithEffect src={item.thumbnailUrl} alt={`아이템 ${item.targetId} 프리뷰 이미지.`} fill />
           </div>
-          <p className="text-body-02 font-regular text-grey-9">{`${item.price.toLocaleString()}원`}</p>
+          <div className="flex flex-col justify-between">
+            <div className="flex flex-col">
+              <p className="text-body-02 text-grey-9 custom-break-words font-medium">{item.title}</p>
+              <p className="text-caption-01 text-grey-6 font-medium">{item.artistName}</p>
+            </div>
+            <p className="text-body-02 font-regular text-grey-9">{`${item.price.toLocaleString()}원`}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
