07586c342622712d088875b47efc5a37136496a3
KWONDU <go203302@gmail.com>
2025-08-04 19:32:22 +0900

    feat: my-profile

--- src/components/features/my/profile/profile-list-with-bottom-fixed-bar.tsx
+++ src/components/features/my/profile/profile-list-with-bottom-fixed-bar.tsx
@@ -1,50 +1,231 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import clsx from 'clsx';
 
-// import { fetchMe, patchMe, patchMeWithdraw } from '@/services/api/member-controller';
-import { fetchMe } from '@/services/api/member-controller';
+import { useAuthStore } from '@/lib/store/use-auth-store';
+import { logoutOnce } from '@/services/api/axios/auth';
+import { fetchMe, patchMe, patchMeWithdraw } from '@/services/api/member-controller';
+import { getQueryClient } from '@/services/query/client';
+import type { MemberUpdateRequestSchema } from '@/types/member-controller';
 
 import BinaryComponent from './binary-components';
 import InputComponent from './input-component';
 import SelectComponent from './select-component';
 
 export default function ProfileListWithBottomFixedBar() {
+  const now = useMemo(() => new Date(), []);
+
+  const [initialData, setInitialData] = useState<MemberUpdateRequestSchema | null>(null);
+
   const [nameValue, setNameValue] = useState('');
   const [zipCodeValue, setZipCodeValue] = useState('');
   const [addressMainValue, setAddressMainValue] = useState('');
   const [addressDetailsValue, setAddressDetailsValue] = useState('');
   const [phoneStartValue, setPhoneStartValue] = useState('010');
-  const [phoneMiddleValue, setPhoneMiddleValue] = useState('0000');
-  const [phoneEndValue, setPhoneEndValue] = useState('0000');
+  const [phoneMiddleValue, setPhoneMiddleValue] = useState('');
+  const [phoneEndValue, setPhoneEndValue] = useState('');
   const [smsAgreeValue, setSmsAgreeValue] = useState(true);
-  const [genderValue, setGenderValue] = useState(true);
-  const [birthYearValue, setBirthYearValue] = useState(0);
-  const [birthMonthValue, setBirthMonthValue] = useState(0);
-  const [birthDayValue, setBirthDayValue] = useState(0);
+  const [genderValue, setGenderValue] = useState<MemberUpdateRequestSchema['gender']>(null);
+  const [birthYearValue, setBirthYearValue] = useState(now.getFullYear());
+  const [birthMonthValue, setBirthMonthValue] = useState(now.getMonth() + 1);
+  const [birthDayValue, setBirthDayValue] = useState(now.getDate());
   const [isSolarValue, setIsSolarValue] = useState(true);
+
+  const [phoneAlertText, setPhoneAlertText] = useState<string | null>(null);
+  const [gendeAlertText, setGenderAlertText] = useState<string | null>(null);
+
+  // non-change values
+  const [recipientNameValue, setRecipientNameValue] = useState('');
+  const [recipientPhoneValue, setRecipientPhoneValue] = useState('');
 
   const { data } = useSuspenseQuery({
     queryKey: ['me'],
     queryFn: fetchMe,
   });
 
+  // set initial data
   useEffect(() => {
-    setNameValue(data.profile.name);
-    setZipCodeValue(data.address?.zipCode ?? '');
-    setAddressMainValue(data.address?.addressMain ?? '');
-    setAddressDetailsValue(data.address?.addressDetails ?? '');
-    setPhoneStartValue(data.profile.phone ? data.profile.phone.split('-')[0] : '010');
-    setPhoneMiddleValue(data.profile.phone ? data.profile.phone.split('-')[1] : '0000');
-    setPhoneEndValue(data.profile.phone ? data.profile.phone.split('-')[2] : '0000');
-    setSmsAgreeValue(data.profile.smsAgree);
-    setGenderValue(data.profile.gender === 'FEMALE' ? true : false);
-    setBirthYearValue(data.birth.birthYear ?? 0);
-    setBirthMonthValue(data.birth.birthMonth ?? 0);
-    setBirthDayValue(data.birth.birthDay ?? 0);
-    setIsSolarValue(data.birth.isSolar ?? true);
-  }, [data]);
+    // profile
+    const { name, phone: rawPhone, smsAgree, gender } = data.profile;
+    const phone = rawPhone ?? '010--';
+    const [start, middle, end] = phone.split('-');
+
+    // birth
+    const {
+      birthYear: rawBirhtYear,
+      birthMonth: rawBirhtMonth,
+      birthDay: rawBirthDay,
+      isSolar: rawIsSoloar,
+    } = data.birth;
+    const birthYear = rawBirhtYear ?? now.getFullYear();
+    const birthMonth = rawBirhtMonth ?? now.getMonth() + 1;
+    const birthDay = rawBirthDay ?? now.getDate();
+    const isSolar = rawIsSoloar ?? true;
+
+    // address
+    const address = data.address ?? {};
+    const { recipientName = '', zipCode = '', addressMain = '', addressDetails = '', recipientPhone = '' } = address;
+
+    setNameValue(name);
+    setPhoneStartValue(start);
+    setPhoneMiddleValue(middle);
+    setPhoneEndValue(end);
+    setSmsAgreeValue(smsAgree);
+    setGenderValue(gender);
+    setBirthYearValue(birthYear);
+    setBirthMonthValue(birthMonth);
+    setBirthDayValue(birthDay);
+    setIsSolarValue(isSolar);
+    setZipCodeValue(zipCode);
+    setAddressMainValue(addressMain);
+    setAddressDetailsValue(addressDetails);
+
+    setRecipientNameValue(recipientName);
+    setRecipientPhoneValue(recipientPhone);
+
+    setInitialData({
+      phone,
+      smsAgree,
+      gender,
+      birth: {
+        year: birthYear,
+        month: birthMonth,
+        day: birthDay,
+        solar: isSolar,
+      },
+      address: {
+        recipientName,
+        zipCode,
+        addressMain,
+        addressDetails,
+        recipientPhone,
+      },
+    });
+  }, [data, now]);
+
+  // handle data change
+  const currentData: MemberUpdateRequestSchema = useMemo(
+    () => ({
+      phone: `${phoneStartValue}-${phoneMiddleValue}-${phoneEndValue}`,
+      smsAgree: smsAgreeValue,
+      gender: genderValue,
+      birth: {
+        year: birthYearValue,
+        month: birthMonthValue,
+        day: birthDayValue,
+        solar: isSolarValue,
+      },
+      address: {
+        recipientName: recipientNameValue,
+        zipCode: zipCodeValue,
+        addressMain: addressMainValue,
+        addressDetails: addressDetailsValue,
+        recipientPhone: recipientPhoneValue,
+      },
+    }),
+    [
+      phoneStartValue,
+      phoneMiddleValue,
+      phoneEndValue,
+      smsAgreeValue,
+      genderValue,
+      birthYearValue,
+      birthMonthValue,
+      birthDayValue,
+      isSolarValue,
+      recipientNameValue,
+      zipCodeValue,
+      addressMainValue,
+      addressDetailsValue,
+      recipientPhoneValue,
+    ],
+  );
+
+  const isModified = useMemo(() => {
+    return JSON.stringify(initialData) !== JSON.stringify(currentData);
+  }, [initialData, currentData]);
+
+  // address search (DAUM)
+  const openAddressSearch = () => {
+    new window.daum.Postcode({
+      oncomplete: (data: { zonecode: string; address: string }) => {
+        setZipCodeValue(data.zonecode);
+        setAddressMainValue(data.address);
+      },
+    }).open();
+  };
+
+  // handle submit
+  const handleSubmit = async () => {
+    // necessary full phone number
+    if ((phoneMiddleValue !== '' && phoneEndValue === '') || (phoneMiddleValue === '' && phoneEndValue !== '')) {
+      setPhoneAlertText('유효한 전화번호를 입력해야 합니다.');
+
+      const element = document.getElementById('phone-value-component');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+
+      return null;
+    }
+    setPhoneAlertText(null);
+
+    // necessary gender information
+    if (genderValue === null) {
+      setGenderAlertText('필수 입력 항목입니다.');
+
+      const element = document.getElementById('gender-value-component');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+
+      return null;
+    }
+    setGenderAlertText(null);
+
+    const payload = {
+      phone: `${phoneStartValue}-${phoneMiddleValue}-${phoneEndValue}`,
+      smsAgree: smsAgreeValue,
+      gender: genderValue,
+      birth: {
+        year: birthYearValue,
+        month: birthMonthValue,
+        day: birthDayValue,
+        solar: isSolarValue,
+      },
+      address: {
+        recipientName: recipientNameValue,
+        zipCode: zipCodeValue,
+        addressMain: addressMainValue,
+        addressDetails: addressDetailsValue,
+        recipientPhone: recipientPhoneValue,
+      },
+    };
+
+    await patchMe(payload);
+
+    const queryClient = getQueryClient();
+    await queryClient.invalidateQueries({ queryKey: ['me'] });
+
+    setGenderAlertText(null);
+  };
+
+  // withdraw
+  const setLoggedIn = useAuthStore((state) => state.setLoggedIn);
+  const setHasChecked = useAuthStore((s) => s.setHasChecked);
+  const logout = async () => {
+    await logoutOnce();
+    setLoggedIn(false);
+    setHasChecked(true);
+    window.location.replace('/');
+  };
+
+  const handleWithDraw = async () => {
+    await patchMeWithdraw();
+    logout();
+  };
 
   return (
     // 35 = 5.5 (margin-bottom) + 21 (bottom-fixed-bar) + 8.5 (indicator height)
@@ -55,7 +236,7 @@ export default function ProfileListWithBottomFixedBar() {
           {/* 이름 */}
           <div className="mx-5 flex flex-col gap-3.5">
             <span className="text-headline-05 text-black">이름</span>
-            <InputComponent value={nameValue} setValue={setNameValue} readOnly />
+            <InputComponent name="이름" value={nameValue} setValue={setNameValue} readOnly />
           </div>
           <hr className="border-t-green border-t" />
           {/* 주소 */}
@@ -63,26 +244,42 @@ export default function ProfileListWithBottomFixedBar() {
             <span className="text-headline-05 text-black">주소 (기본 배송지)</span>
             <div className="flex flex-col gap-2.5">
               <div className="flex gap-3">
-                <InputComponent value={zipCodeValue} setValue={setZipCodeValue} isHighlight />
-                <button className="flex h-11 w-29 items-center justify-center bg-black p-2.5">
-                  <p className="text-body-02 font-regular text-white">우편번호 검색</p>
+                <InputComponent
+                  name="주소 (기본 배송지)"
+                  value={zipCodeValue}
+                  setValue={setZipCodeValue}
+                  readOnly
+                  isHighlight
+                />
+                <button onClick={openAddressSearch} className="cursor-pointer">
+                  <div className="flex h-11 w-29 items-center justify-center rounded-sm bg-black p-2.5">
+                    <p className="text-body-02 font-regular text-white">우편번호 검색</p>
+                  </div>
                 </button>
               </div>
-              <InputComponent value={addressMainValue} setValue={setAddressMainValue} />
-              <InputComponent value={addressDetailsValue} setValue={setAddressDetailsValue} />
+              <InputComponent name="기본 주소" value={addressMainValue} setValue={setAddressMainValue} readOnly />
+              <InputComponent name="세부 주소" value={addressDetailsValue} setValue={setAddressDetailsValue} />
             </div>
           </div>
           <hr className="border-t-green border-t" />
           {/* 휴대전화 */}
-          <div className="mx-5 flex flex-col gap-3.5">
+          <div className="mx-5 flex flex-col gap-4">
             <span className="text-headline-05 text-black">휴대전화</span>
             <div className="flex items-center gap-1.75">
-              <InputComponent value={phoneStartValue} setValue={setPhoneStartValue} isHighlight />
+              <SelectComponent
+                name="시작 전화번호"
+                value={phoneStartValue}
+                setValue={setPhoneStartValue}
+                optionList={['010', '011', '012', '013', '014', '015', '016', '017', '018', '019']}
+                widthClass="flex-1"
+                isHighlight
+              />
               <hr className="w-1.5 border-t border-[#000000]" />
-              <InputComponent value={phoneMiddleValue} setValue={setPhoneMiddleValue} />
+              <InputComponent name="중간 전화번호" value={phoneMiddleValue} setValue={setPhoneMiddleValue} isPhone />
               <hr className="w-1.5 border-t border-[#000000]" />
-              <InputComponent value={phoneEndValue} setValue={setPhoneEndValue} />
+              <InputComponent name="끝 전화번호" value={phoneEndValue} setValue={setPhoneEndValue} isPhone />
             </div>
+            {phoneAlertText && <span className="text-caption-01 text-orange -mt-1 font-bold">{phoneAlertText}</span>}
           </div>
           <hr className="border-t-green border-t" />
           {/* SMS 수신여부 */}
@@ -111,9 +308,16 @@ export default function ProfileListWithBottomFixedBar() {
         </div>
         <div className="flex flex-col gap-6">
           {/* 성별 */}
-          <div className="mx-5 flex flex-col gap-5">
+          <div id="gender-value-component" className="mx-5 flex flex-col gap-5">
             <span className="text-headline-05 text-black">성별</span>
-            <BinaryComponent value={genderValue} setValue={setGenderValue} trueText="여성" falseText="남성" />
+            <BinaryComponent
+              value={genderValue === 'FEMALE'}
+              setValue={(v) => setGenderValue(v ? 'FEMALE' : 'MALE')}
+              trueText="여성"
+              falseText="남성"
+              isNull={genderValue === null}
+            />
+            {gendeAlertText && <span className="text-caption-01 text-orange -mt-2 font-bold">{gendeAlertText}</span>}
           </div>
           <hr className="border-t-green border-t" />
           {/* 생년월일 */}
@@ -122,29 +326,31 @@ export default function ProfileListWithBottomFixedBar() {
             <div className="flex gap-6">
               <div className="flex items-center gap-2">
                 <SelectComponent
+                  name="년도"
                   value={birthYearValue}
                   setValue={setBirthYearValue}
-                  optionList={[]}
+                  optionList={Array.from({ length: 100 }, (_, i) => now.getFullYear() - 99 + i)}
                   widthClass="w-21.25"
-                  isHighlight
                 />
                 <p className="text-body-02 font-medium text-[#000000]">년</p>
               </div>
               <div className="flex items-center gap-2">
                 <SelectComponent
+                  name="월"
                   value={birthMonthValue}
                   setValue={setBirthMonthValue}
-                  optionList={[]}
-                  widthClass="w-15.25"
+                  optionList={Array.from({ length: 12 }, (_, i) => i + 1)}
+                  widthClass="w-16.25"
                 />
                 <p className="text-body-02 font-medium text-[#000000]">월</p>
               </div>
               <div className="flex items-center gap-2">
                 <SelectComponent
+                  name="일"
                   value={birthDayValue}
                   setValue={setBirthDayValue}
-                  optionList={[]}
-                  widthClass="w-15.25"
+                  optionList={Array.from({ length: 31 }, (_, i) => i + 1)}
+                  widthClass="w-16.25"
                 />
                 <p className="text-body-02 font-medium text-[#000000]">일</p>
               </div>
@@ -154,13 +360,19 @@ export default function ProfileListWithBottomFixedBar() {
         </div>
       </div>
       <div className="flex justify-center">
-        <button className="cursor-pointer">
+        <button className="cursor-pointer" onClick={handleWithDraw}>
           <p className="text-grey-5 text-body-02 font-medium underline">회원 탈퇴하기</p>
         </button>
       </div>
       <div className="fixed-component bg-pale-green border-green bottom-0 border px-5 pt-3 pb-11.5 shadow-[0_0_60px_0_rgba(0,0,0,0.10)]">
-        <button className="w-full cursor-pointer">
-          <div className="bg-grey-9 flex h-15 items-center justify-center rounded-xs">
+        <button
+          className={clsx('w-full', isModified && 'cursor-pointer')}
+          disabled={!isModified}
+          onClick={handleSubmit}
+        >
+          <div
+            className={clsx('flex h-15 items-center justify-center rounded-xs', isModified ? 'bg-grey-9' : 'bg-grey-4')}
+          >
             <p className="text-body-01 text-grey-1 font-bold">회원정보 수정</p>
           </div>
         </button>
