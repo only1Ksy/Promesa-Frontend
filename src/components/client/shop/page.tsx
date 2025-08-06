f04881ae63715e086f26ccaaf265147c30622219
KWONDU <go203302@gmail.com>
2025-08-07 05:49:15 +0900

    design: object-cover image

--- src/components/client/shop/page.tsx
+++ src/components/client/shop/page.tsx
@@ -19,7 +19,13 @@ export default function ClientShopPage({ dehydratedState, initialParams }: Clien
     <HydrationBoundary state={dehydratedState}>
       <div className="flex flex-col">
         <div className="bg-green relative flex h-114 items-end justify-center overflow-hidden">
-          <ImageWithLoading src="/images/shop-background.png" alt="프로메사 쇼핑 페이지의 배경 이미지." fill priority />
+          <ImageWithLoading
+            src="/images/shop-background.png"
+            alt="프로메사 쇼핑 페이지의 배경 이미지."
+            fill
+            priority
+            className="object-cover"
+          />
           <div className="pointer-events-none absolute bottom-0 left-0 z-0 h-130/211 w-full bg-gradient-to-b from-[#000000]/0 to-[#000000]" />
           <div className="z-10 mb-12.5 flex flex-col gap-1.5">
             <p className="text-headline-03 text-center text-white">{SHOP_INTRODUCTION_LINE_LIST[0]}</p>
