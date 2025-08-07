4d74e238a3b52f007b5f661e2f0f71ef86b25d27
KWONDU <go203302@gmail.com>
2025-08-08 01:20:55 +0900

    feat: splash page

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,27 +1,5 @@
-import { dehydrate } from '@tanstack/react-query';
-
 import ClientHomePage from '@/components/client/home/page';
-import { fetchExhibitions } from '@/services/api/exhibition-controller';
-import { fetchBrandInfo } from '@/services/api/home-controller';
-import { fetchNowPopularItems } from '@/services/api/item-controller';
-import { createQueryClient } from '@/services/query/server';
 
 export default async function HomePage() {
-  const queryClient = createQueryClient();
-
-  await Promise.all([
-    queryClient.prefetchQuery({ queryKey: ['brandInfo'], queryFn: fetchBrandInfo }),
-    queryClient.prefetchQuery({
-      queryKey: ['onGoingExhibitions'],
-      queryFn: async () => {
-        const data = await fetchExhibitions('ONGOING');
-        return data.map((item) => item.summary);
-      },
-    }),
-    queryClient.prefetchQuery({ queryKey: ['nowPopularItems'], queryFn: fetchNowPopularItems }),
-  ]);
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return <ClientHomePage dehydratedState={dehydratedState} />;
+  return <ClientHomePage />;
 }
