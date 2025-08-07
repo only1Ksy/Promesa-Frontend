08e694dd53274a412e263a1dbf88db4c0658f7a6
only1Ksy <winterksy@gmail.com>
2025-08-08 01:18:35 +0900

    fix: add disabled to cart bottom bar

--- src/components/features/cart/bottom-fixed-bar.tsx
+++ src/components/features/cart/bottom-fixed-bar.tsx
@@ -11,6 +11,7 @@ export default function BottomFixedBar({ handlePurchase, isAvailable }: BottomFi
   return (
     <div className="bg-pale-green home-shadow flex h-21 w-full px-5 pt-3 pb-4">
       <button
+        disabled={!isAvailable}
         className={clsx(
           'text-body-01 w-full rounded-xs py-2 font-bold',
           isAvailable ? 'cursor-pointer bg-black text-white' : 'bg-grey-4 text-grey-1 cursor-not-allowed',
