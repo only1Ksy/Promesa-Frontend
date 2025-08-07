a01898c59a3381470d9bec66f81428094a575011
only1Ksy <winterksy@gmail.com>
2025-08-08 01:30:43 +0900

    fix: minor

--- src/components/features/my/review/write/review-text.tsx
+++ src/components/features/my/review/write/review-text.tsx
@@ -8,7 +8,7 @@ export default function ReviewText({ content, setContent }: ReviewTextAreaProps)
     <div className="flex flex-col gap-1">
       <div className="text-body-02 flex gap-0.5 font-medium">
         <span className="text-black">상세 리뷰를 작성해주세요.</span>
-        <span className="text-grey-5">(선택)</span>
+        <span className="text-grey-5">(필수)</span>
       </div>
       <textarea
         placeholder="어떤 점이 좋았나요? 10자 이상 작성해주세요."
@@ -16,7 +16,7 @@ export default function ReviewText({ content, setContent }: ReviewTextAreaProps)
         onChange={(e) => setContent(e.target.value)}
         className="bg-green text-body-02 font-regular flex h-34 w-90.5 origin-left scale-[0.875] resize-none gap-10 rounded-sm p-3 text-base outline-none"
         style={{
-          width: '114.29%', // 1 / 0.875 = 약 114.29%
+          width: '114.29%',
         }}
       />
     </div>
